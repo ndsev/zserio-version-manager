3170308ae5df6c812083da16d0bb132df9035c25
Joseph Birkner <j.birkner@klebert-engineering.de>
2020-04-24 15:56:01 +0200

    Added updated.sh script to build/bundle zserio JAR and runtime in python package.

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -eu
+
+echo "------------- Building Zserio Bundle -------------"
+echo ""
+
+cd zserio-official
+ant install
+ant -f compiler/extensions/java install
+ant -f compiler/extensions/cpp install
+ant -f compiler/extensions/python install
+ant zserio_bundle.install
+cd ..
+
+echo ""
+echo "---------- Packaging Zserio JAR/Runtime ----------"
+echo ""
+
+cp -rf zserio-official/distr/zserio.jar src/zserio
+cp -rf zserio-official/compiler/extensions/python/runtime/src/zserio/* src/zserio
+echo "from .gen import *" >> src/zserio/__init__.py
+
+echo ""
+echo "---------------- Updating Version ----------------"
+echo ""
+
+ZSERIO_VERSION=$(java -jar zserio-official/distr/zserio.jar -version | sed "s/version //g")
+echo "$ZSERIO_VERSION" > zserio-version.txt
+echo "Zserio version: $ZSERIO_VERSION"
+
+echo ""
+echo "----------------------- Done ---------------------"
+echo ""
